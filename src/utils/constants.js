f7e530de6567c272ceb65d1846c17091f9b2856d
Ashley Danoy <ad2304384@qu.edu.qa>
2026-08-30 10:42:08 +0300

    refactor: improve utilities and constants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,5 +3,5 @@
 // ═════════════════════════════════════════════════════════
 
 export const DEFAULT_BG = '#ffffff';
-export const DEFAULT_ACCENT = '#a234b5';
+export const DEFAULT_ACCENT = '#670D2F';
 export const DEFAULT_NAVBAR = '#262626';
